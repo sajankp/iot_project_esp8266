00c47be2317f0947704b6529386f7742200933a8
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-25 19:39:38 +0530

    Update main to overcome connection issue to be counted

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import uwebsockets.client
-import ujson,time,machine
+import ujson, time, machine, dht
 
 def main():
 	uri = 'ws://35.244.13.244/ws/post'
@@ -14,10 +14,15 @@ def main():
 			mesg = ujson.dumps({'temp' : out.temperature()})
 			websocket.send(mesg)
 			resp = websocket.recv()
-			print(resp)
-			time.sleep(60)
+			print("response : {}".format(resp))
+		except AssertionError:
+			print("connection issue")
+			websocket = uwebsockets.client.connect(uri)
+			print("Connecting to {}: and status is {}".format(uri,websocket.open))
 		except:
 			count += 1
 			print(count)
-
+		finally:
+			print('-'*20)
+			time.sleep(20)
 	websocket.close()
