b6fc062e19a58871da558ffcb6008f8649b90f09
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-26 18:25:27 +0530

    Make use or requests instead of websocket for sending data every 30min

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
-import uwebsockets.client
+# import uwebsockets.client
+import urequests
 import ujson, time, machine, dht
 
 def main():
-	uri = 'ws://35.244.13.244/ws/post'
-	# uri = 'ws://echo.websocket.org/'
-	websocket = uwebsockets.client.connect(uri)
+	uri = 'http://35.244.13.244/post'
+	# uri = 'ws://echo.websocket.org/' #for websocket testing
+	# websocket = uwebsockets.client.connect(uri) #for websocket use
+	led = machine.Pin(16, machine.Pin.OUT)
 	print("Connecting to {}:".format(uri))
 	out = dht.DHT11(machine.Pin(12)) # d6 pin
+	t = 3000
 	count = 0
 	while True:
 		try:
+			t = 60
 			out.measure()
 			print("temp :",out.temperature())
 			mesg = ujson.dumps({'temp' : out.temperature()})
@@ -17,14 +21,23 @@ def main():
 			count += 1
 			print(count)
 		try:
-			websocket.send(mesg)
-			resp = websocket.recv()
+			req = urequests.post(uri,data = mesg)
+			resp = req.text
 			print("response : {}".format(resp))
 		except Exception as e:
 			print(e)
-			print("connection part")
-			websocket = uwebsockets.client.connect(uri)
+			print("Issue with Connecting to server")
+			led.on()
+			time.sleep(1)
+			led.off()
+			time.sleep(2)
+			led.on()
+			time.sleep(1)
+			led.off()
+			time.sleep(2)
+			t -= 6
+			#	websocket = uwebsockets.client.connect(uri) for reconnecting in case closed
 		finally:
 			print('-'*20)
-			time.sleep(45)
-	websocket.close()
+			time.sleep(t)
+	# websocket.close() #finally closing the websocket connection
