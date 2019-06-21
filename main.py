69312e4a51a843c4f9a075a2a4271f1d79027bce
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-06-21 23:21:16 +0530

    Improve security and hide sensitive info from repo Add sensor identification Improve startup code to signal until internet is online. Move password and secutiy key to separate file.

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import server
 import time
-
 import network
 import machine
 
 sta_if = network.WLAN(network.STA_IF)
 sta_if.ifconfig()
 sta_if.active(True)
-sta_if.connect('SKP','sajan.kp')
-if sta_if.isconnected():
-    out = machine.Pin(16, machine.Pin.OUT)
-    out.off()
-    time.sleep(3)
-    out.on()
-    time.sleep(5)
-if not sta_if.isconnected():
-    out = machine.Pin(16, machine.Pin.OUT)
-    for x in range(5):
+
+with open('wifi_secret') as f:
+    a = f.readlines()
+    wifi_name = a[0].strip()
+    wifi_password = a[1].strip()
+
+sta_if.connect(wifi_name, wifi_password)
+out = machine.Pin(16, machine.Pin.OUT)
+
+while (not sta_if.isconnected()):
         out.off()
         time.sleep(2)
         out.on()
         time.sleep(2)
-
-time.sleep(5)
 
 server.main()
--- server.py
+++ server.py
@@ -1,50 +1,68 @@
 # import uwebsockets.client
 import urequests
-import ujson, time, machine, dht
+import ujson
+import time
+import machine
+import dht
+
+with open('secret') as f:
+    secret_key = f.read().strip()
+
 
 def main():
-	uri = 'http://35.244.13.244/post'
-	# uri = 'http://192.168.225.201:8000/post'
-	# uri = 'ws://echo.websocket.org/' #for websocket testing
-	# websocket = uwebsockets.client.connect(uri) #for websocket use
-	led = machine.Pin(16, machine.Pin.OUT)
-	print("Connecting to {}:".format(uri))
-	out = dht.DHT11(machine.Pin(12)) # d6 p
-	FIRST = True
-	while True:
-		try:
-			t = 900
-			out.measure()
-			ERROR_IN_MEASUREMENT = False
-			mesg = ujson.dumps({'temp': out.temperature(), 'humidity': out.humidity(), 'ERROR_IN_MEASUREMENT': ERROR_IN_MEASUREMENT, 'FIRST': FIRST })
-			print(mesg)
-		except:
-			ERROR_IN_MEASUREMENT = True
-			mesg = ujson.dumps({'temp': out.temperature(), 'humidity': out.humidity(), 'ERROR_IN_MEASUREMENT': ERROR_IN_MEASUREMENT, 'FIRST': FIRST })
-			print("error",mesg)
-		try:
-			req = urequests.post(uri,data = mesg)
-			req = ujson.loads(req.content)
-			resp = req['message']
-			print("response : {}".format(resp))
-			if FIRST == True :
-				print("next reading will be taken in {} seconds".format(req['time']))
-				t = req['time']
-				FIRST = False
-		except Exception as e:
-			print(e)
-			print("Issue with Connecting to server")
-			led.on()
-			time.sleep(1)
-			led.off()
-			time.sleep(2)
-			led.on()
-			time.sleep(1)
-			led.off()
-			time.sleep(2)
-			t -= 6
-			#	websocket = uwebsockets.client.connect(uri) for reconnecting in case closed
-		finally:
-			print('-'*20)
-			time.sleep(t)
-	# websocket.close() #finally closing the websocket connection
+    uri = 'http://35.244.13.244/iot/post'
+    # uri = 'http://192.168.225.201:8000/iot/post' # for testing
+    # uri = 'ws://echo.websocket.org/' # for websocket testing
+    # websocket = uwebsockets.client.connect(uri) # for websocket use
+    led = machine.Pin(16, machine.Pin.OUT)
+    print("Connecting to {}:".format(uri))
+    out = dht.DHT11(machine.Pin(12))
+    # d6 p
+    FIRST = True
+    while True:
+        try:
+            t = 1799.3
+            out.measure()
+            ERROR_IN_MEASUREMENT = False
+            mesg = ujson.dumps({'secretkey': secret_key,
+                                'temp': out.temperature(),
+                                'humidity': out.humidity(),
+                                'ERROR_IN_MEASUREMENT': ERROR_IN_MEASUREMENT,
+                                'FIRST': FIRST})
+            print(mesg)
+        except:
+            ERROR_IN_MEASUREMENT = True
+            mesg = ujson.dumps({'secretkey': secret_key,
+                                'temp': out.temperature(),
+                                'humidity': out.humidity(),
+                                'ERROR_IN_MEASUREMENT': ERROR_IN_MEASUREMENT,
+                                'FIRST': FIRST})
+            print("error", mesg)
+            led.off()
+        try:
+            req = urequests.post(uri, data=mesg)
+            req = ujson.loads(req.content)
+            resp = req['message']
+            print("response : {}".format(resp))
+            if not led.value():
+                led.on()
+            if FIRST:
+                print("next reading will be taken in {} seconds".format(req['time']))
+                t = req['time']
+                if t > 100000:
+                    led.off()
+                FIRST = False
+        except Exception as e:
+            print(e)
+            for x in range(3):
+                led.on()
+                time.sleep(2)
+                led.off()
+                time.sleep(2)
+            t -= 6
+            # websocket = uwebsockets.client.connect(uri)
+            # for reconnecting in case it is closed
+        finally:
+            print('-'*20)
+            time.sleep(t)
+    # websocket.close() #finally closing the websocket connection
